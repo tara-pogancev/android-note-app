ad4e0c14ba78cba94f3396578f651b0489123c77
Tara Pogančev <tarapogancev@gmail.com>
2022-08-01 16:32:08 +0200

    fix: theme file

--- Notes By Cats/app/src/main/java/com/tarapogancev/notesbycats/Settings.kt
+++ Notes By Cats/app/src/main/java/com/tarapogancev/notesbycats/Settings.kt
@@ -1,30 +1,55 @@
 package com.tarapogancev.notesbycats
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.CheckBoxPreference
+import android.preference.Preference
 import android.preference.PreferenceActivity
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 
 class Settings : PreferenceActivity() {
 
-    private lateinit var imageView_back: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.prefs)
-/*
-        imageView_back = findViewById(R.id.imageView_back)
-        imageView_back.setOnClickListener(object: View.OnClickListener{
-            override fun onClick(p0: View?) {
-                val intent: Intent = Intent(this@Settings, MainActivity::class.java)
-                startActivity(intent)
+        loadSettings()
+    }
+
+    fun loadSettings() {
+        var sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var darkMode: Boolean = sharedPreferences.getBoolean("DarkTheme", false)
+        if (darkMode) {
+            setTheme(R.style.Theme_NotesByCatsDark)
+        } else {
+            setTheme(R.style.Theme_NotesByCats)
+        }
+
+        var checkBox_nigth: CheckBoxPreference = findPreference("DarkTheme") as CheckBoxPreference
+        checkBox_nigth.setOnPreferenceChangeListener(object: Preference.OnPreferenceChangeListener {
+            override fun onPreferenceChange(p0: Preference?, p1: Any?): Boolean {
+
+                var darkMode: Boolean = p1 as Boolean
+                if (darkMode) {
+                    setTheme(R.style.Theme_NotesByCatsDark)
+                } else {
+                    setTheme(R.style.Theme_NotesByCats)
+                }
+
+                recreate()
+                return true
             }
 
         })
- */
+    }
 
+    override fun onResume() {
+        loadSettings()
+        super.onResume()
     }
 }
